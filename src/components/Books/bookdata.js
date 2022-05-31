fdd91d9fdac918ec857b761fd22ac439aaccbd10
Akshith1998 <akshithpatluri@gmail.com>
2022-05-31 19:00:34 +0530

    Initialize project using Create React App

---
+++ src/components/Books/bookdata.js
@@ -0,0 +1,9 @@
+import "./bookscss.css"
+export let Bookcontent=(props)=>{
+    return(
+        <div className="book">
+            <h2>{props.bookdata.title}</h2>
+            <p>{props.bookdata.author}</p>
+        </div>
+    )
+}
